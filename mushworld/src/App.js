c561b48ef6d2c8111827912363caf7db95f7b524
Ivan Bozveliev <ivanbozveliev@abv.bg>
2021-11-04 12:46:44 +0200

    modify App.js

--- mushworld/src/App.js
+++ mushworld/src/App.js
@@ -3,6 +3,7 @@ import {  Route , Switch} from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 import About from './About';
+import HomePage from './HomePage';
 
 import './App.css';
 
@@ -13,11 +14,11 @@ function App() {
         <div className="loader"><img src="/images/loading.gif" alt="#" /></div>
       </div>
       <Header />
-
       <Switch>
+        <Route path='/' component={HomePage} exact/>
         <Route path="/about" component={About} />
       </Switch>
-      
+
       <Footer />
     </div>
   );
