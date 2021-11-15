a20d1cb40009e5adc9c6e207bfbf83edd8c6c1ee
Ivan Bozveliev <ivanbozveliev@abv.bg>
2021-11-15 14:07:05 +0200

    cofig changes

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,8 @@
+// const connectionString = process.env.DB_CONNECTION;
 const config = {
 
     PORT: 5000,
-    DB_CONNECTION: `mongodb+srv://MERN:ivan1991@my-db.b2vfi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    DB_CONNECTION: "mongodb+srv://MERN:ivan1991@my-db.b2vfi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     COOKIE_NAME: 'USER_NAME',
     SECRET: 'TOP',
     SALT_ROUNDS: 10
@@ -9,3 +10,5 @@ const config = {
 }
 
 module.exports = config;
+
+
